009dd2ab5bbb2a772b0574c85b252bd2d86dafc8
Thannob Aribarg <iamaribarg@gmail.com>
2025-03-24 14:14:21 +0700

    add api create attraction

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -13,3 +13,15 @@ export async function GET(request){
         return NextResponse.json({error:"Failed to fetch"}, {status: 500})
     }
 }
+
+export async function POST(request){
+    try {
+        const {name, detail, coverimage, latitude, longitude} = await request.json();
+        const [result] = await db.query(
+            'INSERT INTO attractions (name, detail, coverimage, latitude, longitude) VALUES (?,?,?,?,?)',[name, detail,coverimage, latitude, longitude]
+        );
+        return NextResponse.json({id:result.insertId,name, detail,coverimage, latitude, longitude}, {status: 200});
+    } catch (error){
+        return NextResponse.json({error: error}, {status: 500})
+    }
+}
