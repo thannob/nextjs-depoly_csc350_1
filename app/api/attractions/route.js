14df245eed4b0a5ac0847284479de074829e575b
Thannob Aribarg <iamaribarg@gmail.com>
2025-03-24 14:30:31 +0700

    add api update attractions

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -25,3 +25,19 @@ export async function POST(request){
         return NextResponse.json({error: error}, {status: 500})
     }
 }
+
+export async function PUT(request){
+    try {
+        const {id, name, detail, coverimage, latitude, longitude} = await request.json();
+        const [result] = await db.query(
+            'UPDATE attractions SET name=?, detail=?, coverimage=?, latitude=?, longitude=? WHERE id = ?',[name, detail,coverimage, latitude, longitude, id]
+        );
+        if (result.affectedRows == 0){
+            return NextResponse.json({error: "ID Not found"}, {status: 404});
+        }
+
+        return NextResponse.json({message: "Updated", id}, {status: 200});
+    } catch (error){
+        return NextResponse.json({error: error}, {status: 500})
+    }
+}
